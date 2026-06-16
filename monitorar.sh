9cf3162f27f606734f1ae3fa8c6ced0cd06880d3
Frederick Moschkowich <fredmosc.dev@gmail.com>
2026-06-15 23:01:34 -0300

    Adiciona TODO de rotacao de log em monitorar.sh

--- monitorar.sh
+++ monitorar.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # Exemplo de script de monitoramento (Second Gear - Shell Script)
 echo "Monitorando serviço..."
+
+# TODO: Implementar rotacao de log (Mover monitorar.log para monitorar.log.bak ao atingir 1MB)
