2ab61f43c5fc0d7831ea9b12c23c67f501317886
Frederick Moschkowich <fredmosc.dev@gmail.com>
2026-06-16 22:58:17 -0300

    Hotfix: ajusta comentario de permissao em backup.sh

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Exemplo de script de backup (Second Gear - Shell Script)
-
+# ajuste de permissao aplicado em hotfix
 echo "Iniciando backup..."
 
 # linha adcionada depois do git add
