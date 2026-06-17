02b9e25ffbcdb66d5253ceee9043b8253ac9545a
Frederick Moschkowich <fredmosc.dev@gmail.com>
2026-06-16 23:02:23 -0300

    Adiciona BACKUP_RETATION_DAYS apos resolver stash pop

--- backup.sh
+++ backup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Exemplo de script de backup (Second Gear - Shell Script)
 # ajuste de permissao aplicado em hotfix
+
+BACKUP_RETENTION_DAY=7
+
 echo "Iniciando backup..."
 
 # linha adcionada depois do git add
