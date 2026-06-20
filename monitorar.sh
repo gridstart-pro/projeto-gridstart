d15d333716a89b3da02504d129da37fb06dcc84b
Frederick Moschkowich <fredmosc.dev@gmail.com>
2026-06-20 11:44:25 -0300

    fix(monitorar): corrige erro de digitacao na mensagem de inicialização

--- monitorar.sh
+++ monitorar.sh
@@ -2,6 +2,8 @@
 # Exemplo de script de monitoramento (Second Gear - Shell Script)
 echo "Monitorando serviço..."
 
+echo "iniciando verificação do sistema de backup"
+
 # mais um commit no monitorar.sh
 # TODO: Implementar rotacao de log (Mover monitorar.log para monitorar.log.bak ao atingir 1MB) Nivel de LOG DEBUG
 # TODO: Implementar rotacao de log (Mover monitorar.log para monitorar.log.bak ao atingir 1MB) Nível de LOG WARNING
